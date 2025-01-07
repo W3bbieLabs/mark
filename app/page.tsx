4b682f5dd13545410f33eda0b7a07fecc2322a1d
Chris <chris35469@gmail.com>
2025-01-07 16:28:08 -0600

    Add top padding

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col gap-2 pt-80">
-        <h2 className="text-lg font-semibold pt-4 text-center">
+        <h2 className="text-lg font-semibold text-center pt-20">
           Mark's Watch List:
         </h2>
         <ul className="list-disc list-inside">
